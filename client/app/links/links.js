26db6f00be2b75d83d854c4bd59a4d46e5339b6f
garrettjoecox <garrettjcox@gmail.com>
2015-03-06 14:57:30 -0800

    Passing a few tests, fixed signup bug

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,15 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+
+  $scope.data = {};
+
+  $scope.getLinks = function(){
+    Links.then(function(data){
+      $scope.data.links = data;
+    });
+  };
+
+  $scope.getLinks();
+
 });
